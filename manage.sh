45f5f17ba46957282e10f11a0f043a6132bde523
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2020-08-10 14:54:42 +0300

    fix port

--- manage.sh
+++ manage.sh
@@ -2,7 +2,7 @@
 
 
 if [ "$1" = "run" ]; then
-  docker-compose up --remove-orphans -d apache
+  docker-compose up --remove-orphans -d apache website
 else
   docker-compose run --rm -T renderd wait_for postgis:5432 -- /manage.py $@
 fi
